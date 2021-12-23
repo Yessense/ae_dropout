71f10096e2958d5bc740ca6ab4028a045895b717
yessense <Akorchemnyj@gmail.com>
2021-12-23 03:05:47 +0300

    Initial commit

---
+++ src/model/encoder.py
@@ -0,0 +1,66 @@
+from typing import Tuple
+
+import numpy as np
+
+import torch
+from torch import nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, image_size: Tuple[int, int, int] = (1, 64, 64), n_features=5):
+        super(Encoder, self).__init__()
+        """
+        Color: white
+        Shape: square, ellipse, heart
+        Scale: 6 values linearly spaced in [0.5, 1]
+        Orientation: 40 values in [0, 2 pi]
+        Position X: 32 values in [0, 1]
+        Position Y: 32 values in [0, 1]
+        """
+
+        # Layer parameters
+        hidden_channels = 32
+        kernel_size = 4
+        hidden_dim = 256
+
+        self.n_features = n_features
+        self.image_size = image_size
+        self.reshape = (hidden_channels, kernel_size, kernel_size)
+
+        n_channels = self.image_size[0]
+
+        # Convolutional layers
+        cnn_kwargs = dict(kernel_size=kernel_size, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(n_channels, hidden_channels, **cnn_kwargs)
+        self.conv2 = nn.Conv2d(hidden_channels, hidden_channels, **cnn_kwargs)
+        self.conv3 = nn.Conv2d(hidden_channels, hidden_channels, **cnn_kwargs)
+        self.conv_64 = nn.Conv2d(hidden_channels, hidden_channels, **cnn_kwargs)
+
+        # Fully connected layers
+        self.lin1 = nn.Linear(np.product(self.reshape), hidden_dim)
+        self.lin2 = nn.Linear(hidden_dim, hidden_dim)
+
+        # Fully connected layers for mean and variance
+        self.mu_logvar_gen = nn.Linear(hidden_dim, 2 * n_features)
+
+        self.activation = torch.nn.GELU()
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        # Convolutional layers with activation
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.conv2(x))
+        x = self.activation(self.conv3(x))
+        x = self.activation(self.conv_64(x))
+
+        # Fully connected layers with ReLu activations
+        x = x.view((batch_size, -1))
+        x = self.activation(self.lin1(x))
+        x = self.activation(self.lin2(x))
+
+        # Fully connected layer for log variance and mean
+        mean_log_var = self.mu_logvar_gen(x)
+        mean, log_var = mean_log_var.view(-1, self.n_features, 2).unbind(-1)
+
+        return mean, log_var
---
+++ src/training/simple training.py
@@ -0,0 +1,218 @@
+from itertools import product
+from random import random, choices
+from typing import List, Union, Tuple
+
+import numpy as np
+import torch
+from sklearn.model_selection import train_test_split
+from torch import nn
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import logging
+
+logging.basicConfig(level=logging.INFO)
+from src.model.vae import VAE
+from src.utils.dataset import get_dataloader
+
+np.set_printoptions(suppress=True)
+
+
+def kld_loss(mean, log_var):
+    loss = - 0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+    return loss
+
+
+def bce_loss(x, reconstruction):
+    loss = torch.nn.BCELoss(reduction='sum')
+    return loss(reconstruction, x)
+
+
+def vae_loss(images_true, images_pred,
+             mean, log_var, beta = 1):
+    bce = bce_loss(images_true, images_pred)
+    kld = kld_loss(mean, log_var)
+
+    return bce + beta * kld, bce, beta * kld
+
+
+def train_model(autoencoder, optimizer, criterion, dataloader, epochs, device):
+    logging.info(f'Start training')
+
+    n_batches = len(dataloader)
+    n_losses = 3
+
+    train_losses = np.zeros((n_epochs, n_losses))
+
+    losses = []
+
+    for epoch in tqdm(range(epochs)):
+        autoencoder.train()
+        train_losses_per_epoch = np.zeros(n_losses)
+
+        for images_batch, parameters_batch in dataloader:
+            # Batches to Cuda
+            images_batch = images_batch.to(device)
+            parameters_batch = parameters_batch.to(device)
+
+            # Zero grad
+            optimizer.zero_grad()
+
+            # Forward pass
+            images_pred, mean, log_var, = autoencoder(images_batch)
+
+            # Loss
+
+            loss = criterion(images_true=images_batch, images_pred=images_pred,
+                             mean=mean, log_var=log_var)
+
+            loss[0].backward()
+            for l in range(n_losses):
+                train_losses_per_epoch[l] = loss[l].item()
+
+            optimizer.step()
+        print(f'\n{train_losses_per_epoch}')
+
+        look_on_results(autoencoder, dataloader, device)
+    return train_losses
+
+
+def look_on_results(autoencoder, dataloader, device, n_to_show=6):
+    autoencoder.eval()
+    with torch.no_grad():
+        for images_batch, parameters_batch in dataloader:
+            reconstruction, mean, log_var = autoencoder(images_batch.to(device))
+            result = reconstruction.cpu().detach().numpy()
+            ground_truth = images_batch.numpy()
+            break
+
+    plt.figure(figsize=(8, 20))
+    for i, (gt, res) in enumerate(zip(ground_truth[:n_to_show], result[:n_to_show])):
+        plt.subplot(n_to_show, 2, 2 * i + 1)
+        plt.imshow(gt.transpose(1, 2, 0), cmap='gray')
+        plt.subplot(n_to_show, 2, 2 * i + 2)
+        plt.imshow(res.transpose(1, 2, 0), cmap='gray')
+
+    plt.show()
+
+
+def save_model(autoencoder: VAE, path: str = './model.pt') -> None:
+    torch.save(autoencoder.state_dict(), path)
+
+
+def load_model(path: str = './model.pt', latent_dim: int = 5) -> VAE:
+    model: VAE = VAE(latent_dim=latent_dim)
+    model.load_state_dict(torch.load(path))
+    return model
+
+
+def plt_images(*images):
+    plt.figure(figsize=(20, 8))
+    for i, img in enumerate(images):
+        plt.subplot(1, len(images), i + 1)
+        plt.imshow(img.squeeze(0).cpu().detach().numpy().transpose(1, 2, 0), cmap='gray')
+    plt.show()
+
+
+def plt_latent_operations(model, x1, x2, n_features):
+    plt.figure(figsize=(40, 40))
+
+    sampled_x1 = model.get_latent_vector(x1)
+    sampled_x2 = model.get_latent_vector(x2)
+
+    decoded_x1 = model.decode_latent(sampled_x1)
+    decoded_x2 = model.decode_latent(sampled_x2)
+
+    plt_images(x1, decoded_x1, x2, decoded_x2)
+
+    # properties = choices(list(product([True, False], repeat=5)), k=5)
+    properties = []
+    for i in range(5):
+        sample = [True] * 5
+        sample[i] = False
+        properties.append(sample)
+
+    n_features = len(properties)
+
+    for i in range(n_features):
+        result_vector = model.latent_operations(x1, x2, properties[i])
+        plt.subplot(n_features, 3, 3 * i + 1)
+        plt.imshow(x1.squeeze(0).cpu().detach().numpy().transpose(1, 2, 0), cmap='gray')
+        plt.subplot(n_features, 3, 3 * i + 2)
+        plt.imshow(result_vector.squeeze(0).cpu().detach().numpy().transpose(1, 2, 0), cmap='gray')
+        plt.subplot(n_features, 3, 3 * i + 3)
+        plt.imshow(x2.squeeze(0).cpu().detach().numpy().transpose(1, 2, 0), cmap='gray')
+    plt.show()
+
+
+if __name__ == '__main__':
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+    RESUME_TRAINING = True
+
+    bce = 1.
+    kld = 1.
+    feature = 1.
+    latent_dim = 5
+    lr = 0.001
+    n_epochs = 10
+    n_features = 5
+    image_size: Tuple[int, int, int] = (1, 64, 64)
+    batch_size = 256
+
+    logging.info(f"Device: {device}")
+    logging.info(f"Epochs: {n_epochs}")
+    logging.info(f"Latent dim: {latent_dim}")
+    logging.info(f'Creating model')
+
+    # ------------------------------------------------------------
+    # train
+    # ------------------------------------------------------------
+    logging.info(f'Setting up dataloader')
+    # dataloader = get_dataloader('dsprites', batch_size=batch_size)
+    #
+    autoencoder = load_model(latent_dim=latent_dim) if RESUME_TRAINING else VAE(latent_dim=latent_dim, n_features=5)
+    autoencoder = autoencoder.to(device)
+    #
+    # optimizer = torch.optim.Adam(autoencoder.parameters(), lr=lr)
+    # criterion = vae_loss
+    #
+    #
+    # losses = train_model(autoencoder=autoencoder,
+    #                      optimizer=optimizer,
+    #                      criterion=criterion,
+    #                      dataloader=dataloader,
+    #                      epochs=n_epochs,
+    #                      device=device)
+    # save_model(autoencoder)
+
+    # ------------------------------------------------------------
+    # test
+    # ------------------------------------------------------------
+
+    dataloader = get_dataloader('dsprites', batch_size=2)
+
+    image, values = next(iter(dataloader))
+    x1 = image[:1]
+    x2 = image[1:2]
+
+    x1 = x1.to(device)
+    x2 = x2.to(device)
+
+    sampled_x1 = autoencoder.get_latent_vector(x1)
+    sampled_x2 = autoencoder.get_latent_vector(x2)
+
+    # decoded = []
+    # for i in range(5):
+    #     decoded_x = autoencoder.decode_latent(sampled_x1[:, i])
+    #     decoded.append(decoded_x)
+    # for i in range(5):
+    #     decoded_x = autoencoder.decode_latent(sampled_x2[:, i])
+    #     decoded.append(decoded_x)
+    # plt_images(*decoded)
+
+    plt_latent_operations(autoencoder, x1, x2, n_features)
+
+    # look_on_results(model, dataloader, device)
+
+    print("Done")
